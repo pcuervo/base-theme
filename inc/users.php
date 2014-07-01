c7eae1781748f22f8c05978194ce3a59fb866c49
raul <raul@zamacona.me>
2014-07-01 16:34:23 -0500

    usuarios cuervo

--- inc/users.php
+++ inc/users.php
@@ -21,8 +21,8 @@
 		$contactmethods['facebook'] = 'Facebook';
 		return $contactmethods;
 	});
-	
-	
+
+
 	add_action('admin_menu', function() use (&$current_user){
 		if ( in_array('developer', $current_user->roles) ){
 			add_options_page(__('All Settings'), __('All Settings'), 'developer', 'options.php');
@@ -36,8 +36,8 @@
 
 
 	add_action('init', function(){
-		$users = array('alex', 'cova', 'clau', 'juan', 'nori', 'pam', 'york');
-		array_map('create_usuario_maquilador', $users);
+		$users = array('raul', 'miguel', 'alejandro');
+		array_map('create_usuario_cuervo', $users);
 	});
 
 
@@ -45,11 +45,11 @@
 	 * Crear un nuevo usuario
 	 * @param  string $user username
 	 */
-	function create_usuario_maquilador($user){
+	function create_usuario_cuervo($user){
 		$password = wp_generate_password();
-		$user_id  = wp_create_user( $user, $password, "$user@losmaquiladores.com" );
+		$user_id  = wp_create_user( $user, $password, "$user@pcuervo.com" );
 		if ( is_int($user_id) ){
-			set_maquilador_role( $user_id );
+			set_cuervo_role( $user_id );
 			wp_new_user_notification( $user_id, $password );
 		}
 	}
@@ -59,7 +59,7 @@
 	 * Set user role as developer (super admin)
 	 * @param int $user_id
 	 */
-	function set_maquilador_role($user_id){
+	function set_cuervo_role($user_id){
 		$wp_user = get_user_by( 'id', $user_id );
 		$wp_user->set_role( 'developer' );
 	}
